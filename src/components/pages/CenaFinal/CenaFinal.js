10cb6fed3a9e233d8c94529cde1ded7a0f43c330
Nícola Cunha Gonçalves <Nicola.cgoncalves@gmail.com>
2025-12-13 18:40:30 -0300

    Atualização Final

--- src/components/pages/CenaFinal/CenaFinal.js
+++ src/components/pages/CenaFinal/CenaFinal.js
@@ -4,8 +4,10 @@
   ================
 */
 
-import styles from '../../../styles/MenuInicial.module.css';
+import styles from '../../../styles/CenaFinal.module.css';
 import MatrixBackground from '../MenuInicial/MatrixBackground.jsx';
+
+import qrcode from '../../../assets/QrCode.png';
 
 function CenaFinal() {
   return (
@@ -15,11 +17,15 @@ function CenaFinal() {
 
       {/* Camada de scanline */}
       <div className={styles.scanline}></div>
-
+      
       {/* Quadro principal do menu */}
       <div className={styles.menu}>
         <div className={styles.logo}>
           <h1>OBRIGADO POR JOGAR!</h1>
+        </div>
+
+        <div className={styles.qr}>
+          <img src={qrcode} alt="Qr Code" className={styles.img}/>
         </div>
       </div>
     </div>
